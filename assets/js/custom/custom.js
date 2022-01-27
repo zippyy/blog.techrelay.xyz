00702b89edda889e8d7ab9182c814cce0b4bae9f
Zippy <zippyy@zoho.com>
2022-01-26 17:12:07 -0700

    Create custom.js

---
+++ assets/js/custom/custom.js
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+<script type="text/javascript">
+  algoliasearchNetlify({
+    appId: 'N8YM1FVCFC',
+    apiKey: 'c4ce6b0543a51ce5415424a9ea373bf0',
+    siteId: '465a6cb8-7dcd-4672-8003-651ad9c72cc1',
+    branch: 'master',
+    selector: 'div#search',
+  });
+</script>
